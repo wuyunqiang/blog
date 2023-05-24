3c4063b3bce5634301ab659ffa5341210b002d87
wuyunqiang <wuyunqiang@kuaishou.com>
2023-05-24 18:39:02 +0800

    feat: promise

--- src/promise.js
+++ src/promise.js
@@ -4,16 +4,16 @@ const State = {
   rejected: "rejected",
 };
 class MyPromise {
-  state = State.pending;
-  res = null;
-  handles = [];
+  #state = State.pending;
+  #res = null;
+  #handles = [];
 
   constructor(executor) {
     const resolve = (data) => {
-      this.changeState(State.fulfilled, data);
+      this.#changeState(State.fulfilled, data);
     };
     const reject = (reason) => {
-      this.changeState(State.rejected, reason);
+      this.#changeState(State.rejected, reason);
     };
     try {
       executor(resolve, reject);
@@ -22,16 +22,16 @@ class MyPromise {
     }
   }
 
-  changeState = (state, res) => {
-    if (this.state !== State.pending) {
+  #changeState = (state, res) => {
+    if (this.#state !== State.pending) {
       return;
     }
-    this.state = state;
-    this.res = res;
-    this.runMicrotask();
+    this.#state = state;
+    this.#res = res;
+    this.#runMicrotask();
   };
 
-  isPromiseLike = (promise) => {
+  #isPromiseLike = (promise) => {
     return (
       typeof promise !== null &&
       (typeof promise === "object" || typeof promise === "function") &&
@@ -39,15 +39,15 @@ class MyPromise {
     );
   };
 
-  runOne = (callback, resolve, reject) => {
+  #runOne = (callback, resolve, reject) => {
     try {
-      const nextBack = this.state === State.fulfilled ? resolve : reject;
+      const nextBack = this.#state === State.fulfilled ? resolve : reject;
       if (typeof callback !== "function") {
-        nextBack(this.res);
+        nextBack(this.#res);
         return;
       }
-      const data = callback(this.res);
-      if (this.isPromiseLike(data)) {
+      const data = callback(this.#res);
+      if (this.#isPromiseLike(data)) {
         data.then(resolve, reject);
         return;
       }
@@ -56,31 +56,31 @@ class MyPromise {
       reject(error);
     }
   };
-  run = () => {
-    if (this.state === State.pending || !this.handles.length) {
+  #run = () => {
+    if (this.#state === State.pending || !this.#handles.length) {
       return;
     }
-    const item = this.handles.shift();
+    const item = this.#handles.shift();
     const { onFulfilled, onRejected, resolve, reject } = item;
-    this.runOne(
-      this.state === State.fulfilled ? onFulfilled : onRejected,
+    this.#runOne(
+      this.#state === State.fulfilled ? onFulfilled : onRejected,
       resolve,
       reject
     );
-    this.run();
+    this.#run();
   };
 
   /**
    * 运行于微任务中
    * */
-  runMicrotask = () => {
-    queueMicrotask(this.run);
+  #runMicrotask = () => {
+    queueMicrotask(this.#run);
   };
 
   then(onFulfilled, onRejected) {
     return new MyPromise((resolve, reject) => {
-      this.handles.push({ resolve, reject, onFulfilled, onRejected });
-      this.runMicrotask();
+      this.#handles.push({ resolve, reject, onFulfilled, onRejected });
+      this.#runMicrotask();
     });
   }
 
@@ -110,7 +110,7 @@ class MyPromise {
       _resolve = resolve;
       _reject = reject;
     });
-    if (p.isPromiseLike(data)) {
+    if (p.#isPromiseLike(data)) {
       data.then(_resolve, _reject);
     } else {
       _resolve(data);
@@ -206,40 +206,41 @@ class MyPromise {
   }
 }
 
-// const p = new MyPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("aaaaa");
-//     reject("xxxxx");
-//   }, 3000);
-// });
-// const p4 = p
-//   .then(
-//     (data) => {
-//       console.log("test data 111:", data);
-//       return Promise.resolve("bbbbb");
-//     },
-//     (error) => {
-//       console.log("test err 111:", error);
-//     }
-//   )
-//   .then((data) => {
-//     console.log("test data 33333:", data);
-//     return "ccccc";
-//   })
-//   .then((data) => {
-//     console.log("test data 444444:", data);
-//     return Promise.reject("ddddd");
-//   })
-//   .then(
-//     () => {},
-//     (error) => {
-//       console.log("test error 5555:", error);
-//     }
-//   );
+const p = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("aaaaa");
+    reject("xxxxx");
+  }, 3000);
+});
+const p4 = p
+  .then(
+    (data) => {
+      console.log("test data 111:", data);
+      return Promise.resolve("bbbbb");
+    },
+    (error) => {
+      console.log("test err 111:", error);
+    }
+  )
+  .then((data) => {
+    console.log("test data 33333:", data);
+    return "ccccc";
+  })
+  .then((data) => {
+    console.log("test data 444444:", data);
+    return Promise.reject("ddddd");
+  })
+  .then(
+    () => {},
+    (error) => {
+      console.log("test error 5555:", error, p);
+    }
+  );
 
-// p4.then(() => {
+  const sourceP = Promise.resolve()
+  console.log("sourceP:", sourceP);
 
-// });
+// p4.then(() => {});
 // p4.then(() => {});
 // p4.then(() => {});
 
@@ -252,13 +253,13 @@ class MyPromise {
 //   }
 // );
 
-const p1 = MyPromise.resolve(111);
-const p2 = MyPromise.reject(2222);
-const p3 = MyPromise.resolve(33333);
-MyPromise.allSettled([p1, p2, p3])
-  .then((res) => {
-    console.log("res:", res);
-  })
-  .catch((err) => {
-    console.log("err:", err);
-  });
+// const p1 = MyPromise.resolve(111);
+// const p2 = MyPromise.reject(2222);
+// const p3 = MyPromise.resolve(33333);
+// MyPromise.allSettled([p1, p2, p3])
+//   .then((res) => {
+//     console.log("res:", res);
+//   })
+//   .catch((err) => {
+//     console.log("err:", err);
+//   });
