8fb90444a86df9431a096625640559afe72b1197
wuyunqiang <wuyunqiang@kuaishou.com>
2024-07-24 19:57:18 +0800

    feat: add count down

---
+++ example/react-native-count-down/styles.ts
@@ -0,0 +1,15 @@
+import { PlusStyle } from '@mfe/plus';
+
+export const useStyles = PlusStyle.create({
+    countDown: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    },
+    text: {
+        color: '#FF5477',
+        fontFamily: 'PingFang SC',
+        fontSize: 12,
+        fontWeight: '400',
+    },
+});
